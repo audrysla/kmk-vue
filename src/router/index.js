f0eba5df4efddfd588093e5615e4cf8f4d417c14
audrysla <kmkyou1@nate.com>
2024-05-08 17:00:45 +0900

    라우터 모드 히스토리 추가

--- src/router/index.js
+++ src/router/index.js
@@ -6,16 +6,22 @@ import Sub1 from '@/components/Sub1'
 import Sub2 from '@/components/Sub2'
 import Sub3 from '@/components/Sub3'
 
-const routes = [
-    { path : "/", name : "MainPage", component : Main },
-    { path : "/sub1", name : "SubPage1", component : Sub1 },
-    { path : "/sub2", name : "SubPage2", component : Sub2 },
-    { path : "/sub3", name : "SubPage3", component : Sub3 }
-]
+// const routes = [
+//     { path : "/", name : "MainPage", component : Main },
+//     { path : "/sub1", name : "SubPage1", component : Sub1 },
+//     { path : "/sub2", name : "SubPage2", component : Sub2 },
+//     { path : "/sub3", name : "SubPage3", component : Sub3 }
+// ]
 
 const router = createRouter({
+    mode: 'history',
     history : createWebHistory(),
-    routes
+    routes : [
+        { path : "/", name : "MainPage", component : Main },
+        { path : "/sub1", name : "SubPage1", component : Sub1 },
+        { path : "/sub2", name : "SubPage2", component : Sub2 },
+        { path : "/sub3", name : "SubPage3", component : Sub3 }
+    ]
 })
 
 export default router;
